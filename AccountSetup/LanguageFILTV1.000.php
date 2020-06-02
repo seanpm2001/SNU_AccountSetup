410e2e79e46ff4d40311ff4efd2d4feb6cdb85e5
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-02 14:53:18 -0700

    3 language filter PHP files
    3 PHP files for language filtering.

---
+++ AccountSetup/LanguageFILTV1.000.php
@@ -0,0 +1,79 @@
+<?php // start of file
+// start of script
+// language during setup
+/* language list */
+// UTF RANGE: ANSI-UTF-8
+$English = "English"; // 1
+$Spanish = "español"; // 2
+$French = "Français"; // 3
+$German = ""; // 4
+$Japanese = ""; // 5
+$Russian = ""; // 6
+$Korean = ""; // 7
+$Italian = ""; // 8
+$Polish = "Polski"; // 9
+$Chinese = ""; // 10
+$Portuguese = ""; // 11
+$Arabic = ""; // 12
+$Swedish = ""; // 13
+$Punjabi = ""; // 14
+$Vietnamese = ""; // 15
+/* convert text to different languages */
+// convert "language" to other languages and back
+$LanguageTXT_TITLE__English = "Language";
+$LanguageTXT_TITLE__Spanish = "";
+$LanguageTXT_TITLE__French = "";
+$LanguageTXT_TITLE__German = "";
+$LanguageTXT_TITLE__Japanese = "";
+$LanguageTXT_TITLE__Russian = "";
+$LanguageTXT_TITLE__Korean = "";
+$LanguageTXT_TITLE__Italian = "";
+$LanguageTXT_TITLE__Polish = "";
+$LanguageTXT_TITLE__Chinese = "";
+$LanguageTXT_TITLE__Portuguese = "";
+$LanguageTXT_TITLE__Arabic = "";
+$LanguageTXT_TITLE__Swedish = "";
+$LanguageTXT_TITLE__Punjabi = "";
+$LanguageTXT_TITLE__Vietnamese = "";
+/* end of section */
+// convert "VOCAL SETUP DISABLED" to other languages and back
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__ENGLISH = "VOCAL SETUP DISABLED";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__SPANISH = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__FRENCH = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__GERMAN = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__JAPANESE = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__RUSSIAN = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__KOREAN = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__ITALIAN = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__POLISH = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__CHINESE = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__PORTUGUESE = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__ARABIC = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__SWEDISH = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__PUNJABI = "";
+$LanguageTXT_HEADING_VOCAL_SETUP_DISABLED__VIETNAMESE = "";
+/* end of section */
+// convert "CONTINUE" to other languages and back
+$LanguageTXT_HEADING_CONTINUE__ENGLISH = "CONTINUE";
+$LanguageTXT_HEADING_CONTINUE__SPANISH = "";
+$LanguageTXT_HEADING_CONTINUE__FRENCH = "";
+$LanguageTXT_HEADING_CONTINUE__GERMAN = "";
+$LanguageTXT_HEADING_CONTINUE__JAPANESE = "";
+$LanguageTXT_HEADING_CONTINUE__RUSSIAN = "";
+$LanguageTXT_HEADING_CONTINUE__KOREAN = "";
+$LanguageTXT_HEADING_CONTINUE__ITALIAN = "";
+$LanguageTXT_HEADING_CONTINUE__POLISH = "";
+$LanguageTXT_HEADING_CONTINUE__CHINESE = "";
+$LanguageTXT_HEADING_CONTINUE__PORTUGUESE = "";
+$LanguageTXT_HEADING_CONTINUE__ARABIC = "";
+$LanguageTXT_HEADING_CONTINUE__SWEDISH = "";
+$LanguageTXT_HEADING_CONTINUE__PUNJABI = "";
+$LanguageTXT_HEADING_CONTINUE__VIETNAMESE = "";
+/* end of section */
+// end of conversion phase 
+print ("Hello World");
+/* Finalization */
+// instructions to finalize the output and overwrite process
+888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+/* end of section */
+// end of script ?>
